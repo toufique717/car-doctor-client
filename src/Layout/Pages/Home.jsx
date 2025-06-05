29d916c9c621a79c25c65e89e694765efd3aaede
toufique717 <toufiquehossain716.com>
2025-06-05 11:23:46 +0600

    uy

--- src/Layout/Pages/Home.jsx
+++ src/Layout/Pages/Home.jsx
@@ -4,6 +4,8 @@ import image2 from '../../assets/2.jpg'
 import image3 from '../../assets/3.jpg'
 import image4 from '../../assets/4.jpg'
 import image5 from '../../assets/5.jpg'
+import person from '../../assets/person.jpg'
+import parts from  '../../assets/parts.jpg'
 
 const Home = () => {
     return (
@@ -115,6 +117,44 @@ const Home = () => {
 
                 </div> {/* Close carousel */}
             </div> {/* Close wrapper */}
+
+
+
+<div>
+    {/* About div  */}
+
+
+    <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row">
+     <div className='w-1/2 relative'>
+        <img
+      src={person}
+      className=" w-3/4 rounded-lg shadow-2xl" />
+
+      <img
+      src={parts}
+      className=" absolute right-5 top-36   border-8 border-white-800  w-1/2 rounded-lg shadow-2xl" />
+     </div>
+    <div  className='w-1/2'>
+    <p className='text-xl font-bold text-orange-500'>About us</p>
+      <h1 className="text-5xl font-bold"> We Are qylified & Of Experient in This Field</h1>
+      <p className="py-6">
+       
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary bg-orange-500 border-orange-500"> Get More Info</button>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
         </div> // Close outer container
     );
 };
